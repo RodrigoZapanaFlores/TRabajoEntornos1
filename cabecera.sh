6da879b950b84a33d9bb9fff690d8de800229fef
Rodrigo Zapana <rodrigozapanaflores@gmail.com>
2024-11-15 18:44:05 +0100

    files.sh

---
+++ cabecera.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+cat << EOF 
+
+<!doctype html>
+<html>
+<head>
+  <meta charset='utf-8'>
+  <title>$1</title>
+    <link rel="stylesheet" href="w3.css"> 
+</head>
+<body>
+<header class='w3-blue'>
+    <p>Cabecera</p>
+</header>
+EOF
---
+++ pie.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+cat << EOF
+<div class='w3-card'>
+    <h1>Hola Mundo</h1>
+</div>
+<h1>AAAAAAAAAAAAA</h1>
+<footer class='w3-sand'>
+    <p> Pie de pagina </p>
+    <p>&copy; 2024 Rodrigo Zapana. Todos los derechos reservados.</p>
+</footer>
+</body>
+</html>
+
+EOF
